8fffc2545da70e19e5414115572bff76b6592226
deepirex <73782943+deepirex@users.noreply.github.com>
2023-04-09 19:10:39 +0530

    Commit message

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,6 +1,6 @@
 import json
 import pytest
-from helloworld.application import application
+from application import application
 
 
 @pytest.fixture
@@ -10,7 +10,6 @@ def client():
 
 def test_response(client):
     result = client.get()
-    response_body = json.loads(result.get_data())
+    response_body = result.get_data()
+    print(response_body)
     assert result.status_code == 200
-    assert result.headers['Content-Type'] == 'application/json'
-    assert response_body['Output'] == 'Hello World'
