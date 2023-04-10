13a6f9bea6e6848a7cb9dfb5acdc305a16b33d22
deepirex <73782943+deepirex@users.noreply.github.com>
2023-04-10 12:14:01 +0530

    Update test_application.py to specify template folder

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,18 +1,19 @@
+import os
 import json
 import pytest
 import sys
 from pathlib import Path
+from flask import Flask
 
 # Add the project root to the Python path
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
-from application import application
-
-# Rest of the test code
 @pytest.fixture
 def client():
-    return application.test_client()
-
+    app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '..', 'templates'))
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
 
 def test_response(client):
     result = client.get()
