e0ba4a90fb16fcec7e03bf5dc11e4399db7079e4
deepirex <73782943+deepirex@users.noreply.github.com>
2023-04-10 12:40:27 +0530

    Update test_application.py to specify the correct route

--- tests/test_application.py
+++ tests/test_application.py
@@ -4,20 +4,22 @@ import pytest
 import sys
 from pathlib import Path
 from flask import Flask
+from application import application
 
 # Add the project root to the Python path
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
 @pytest.fixture
 def client():
-    app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '..', 'templates'))
-    app.config['TESTING'] = True
-    with app.test_client() as client:
+    application.config['TESTING'] = True
+    with application.test_client() as client:
         yield client
+
 
 def test_response(client):
     result = client.get('/')
     response_body = result.get_data()
     print(response_body)
     assert result.status_code == 200
+
 
